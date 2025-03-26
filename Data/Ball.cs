d1ea63694c7bb96cfa98cd199b19ed7a2f7e8bb5
bkolacinski <bartoszkolacinski4@gmail.com>
2025-03-26 20:43:50 +0100

    completed etap 0

---
+++ Data/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Data
+{
+    public class Ball
+    {
+        public int Id { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public Vector2 Velocity { get; set; }
+        public double Radius { get; set; }
+        
+        public void Move(double deltaTime)
+        {
+            PositionX += Velocity.X * deltaTime;
+            PositionY += Velocity.Y * deltaTime;
+        }
+    }
+}
---
+++ DataTest/BallTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using Data;
+
+namespace DataTest;
+
+[TestClass]
+public class BallTest
+{
+    [TestMethod]
+    public void Move_UpdatesPosition_AccordingToVelocityAndDeltaTime()
+    {
+        var ball = new Ball
+        {
+            PositionX = 0,
+            PositionY = 0,
+            Velocity = new Vector2(1, 2)
+        };
+        
+        double deltaTime = 1.5;
+        ball.Move(deltaTime);
+        
+        Assert.AreEqual(0 + 1 * deltaTime, ball.PositionX, 0.0001);
+        Assert.AreEqual(0 + 2 * deltaTime, ball.PositionY, 0.0001);
+    }
+    
+    [TestMethod]
+    public void Move_NoDeltaTime_PositionRemainsTheSame()
+    {
+        var ball = new Ball
+        {
+            PositionX = 4,
+            PositionY = 20,
+            Velocity = new Vector2(1, 1)
+        };
+        
+        double deltaTime = 0;
+        ball.Move(deltaTime);
+        
+        Assert.AreEqual(4, ball.PositionX, 0.0001);
+        Assert.AreEqual(20, ball.PositionY, 0.0001);
+    }
+}
